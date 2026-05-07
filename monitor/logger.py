03f22c25658daf7536db05834b64b8567a8ad4bf
Movses <movsesmargarayan339@gmail.com>
2026-05-07 18:43:54 +0100

    Simplify main entrypoint architecture

--- monitor/logger.py
+++ monitor/logger.py
@@ -1,4 +1,26 @@
-def log(message):
-    
-    with open("logs/logs.txt", "a") as f:
-        f.write(message + "\n")
+from datetime import datetime
+
+LOG_LEVEL_STDOUT = True
+LOG_LEVEL_FILE = True
+
+LOG_FILE_PATH = "logs/logs.txt"
+
+
+def get_timestamp():
+    return datetime.utcnow().isoformat() + "Z"
+
+
+def log(message, level="INFO"):
+
+    timestamp = get_timestamp()
+
+    formatted_message = f"{timestamp} [{level}] {message}"
+
+    # console output
+    if LOG_LEVEL_STDOUT:
+        print(formatted_message)
+
+    # file output
+    if LOG_LEVEL_FILE:
+        with open(LOG_FILE_PATH, "a") as f:
+            f.write(formatted_message + "\n")
